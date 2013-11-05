693f731dc67ab9d51538c3d3b009eab8f7355ff7
Hector <alu0100608939@ull.edu.es>
2013-11-05 12:40:17 +0000

    Añadido fichero matrixchero matrix_spec.rb

---
+++ spec/matrix_spec.rb
@@ -0,0 +1,40 @@
+require "./lib/matrix_main.rb"
+
+
+describe Matrix do
+# before :each do
+#   @f1 = Matrix.new()
+# end
+# 
+# describe "Almacenamiento de las matrices" do
+#   it "El elemento 0,0 de la matriz se almacena correctamente" do
+#   
+#   end
+#   it "Los elementos de la matriz se almacenan correctamente" do
+#     
+#   end
+# end
+#
+# describe "Operaciones con una matriz" do
+#  it "La traspuesta de la matriz se almacena correctamente" do
+#     
+#   end
+#   
+#   it "La	de la matriz se almacena correctamente" do
+#     
+#   end
+# end
+# 
+# describe "Operaciones con dos matrices" do
+#   it "La suma de matrices se realiza correctamente" do
+#     
+#   end
+#   
+#    it "La resta de matrices se realiza correctamente" do
+#      
+#    end
+#    it "El producto de matrices se realiza correctamente" do
+#    
+#    end
+#  end
+end
