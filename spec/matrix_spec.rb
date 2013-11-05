6e8fe07b10d7cac9d65a7928676b293726b779f1
Hector <alu0100608939@ull.edu.es>
2013-11-05 16:00:31 +0000

    Añadidas expectativas para contructor y nº filas y columnas

--- spec/matrix_spec.rb
+++ spec/matrix_spec.rb
@@ -1,11 +1,27 @@
-require "./lib/matrix_main.rb"
+require "./lib/matrix.rb"
 
 
-describe Matrix do
-# before :each do
-#   @f1 = Matrix.new()
-# end
-# 
+describe Matriz do
+  
+ before :each do
+   @f1 = Matriz.new([[1,2],[3,4],[5,6]])
+ end
+ 
+ describe "La matriz se genera correctamente" do
+   it "La matriz tiene formato correcto" do
+     f = Matriz.new([[1,2],[3]]).should raise_error
+     f_2 = Matriz.new([[1,2],[3,4]]).should_not raise_error
+   end
+ 
+ describe "Almacenamiento de nº de filas y columnas" do
+  it "El numero de filas es correcto" do
+  @f1.filas.should eq(3)
+  end
+  it "El numero de columnas es correcto" do
+  @f1.columnas.should eq(2)
+  end
+ end
+ 
 # describe "Almacenamiento de las matrices" do
 #   it "El elemento 0,0 de la matriz se almacena correctamente" do
 #   
