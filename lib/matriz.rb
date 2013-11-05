328bd00d18d254fd2c1da6937f60b7433e05bacb
Elías Rodríguez Martín <alu0100456608@ull.edu.es>
2013-11-05 16:12:18 +0000

    Implenetadas funcionalidades y arreglado spec

--- lib/matriz.rb
+++ lib/matriz.rb
@@ -1,8 +1,18 @@
 class Matriz
         attr_reader :filas, :columnas
 
-	def initialize(elementos)
-
-	end
+        #Inicialización de la matriz
+        def initialize(elementos)
+                #Se copian los elementos
+                @elementos = Array.new(elementos);
+                #Se calculan el número de filas y columnas
+                @filas = @elementos.length
+                @columnas = @elementos[0].length
+                #Se comprueba que los elementos pasados se corresponden con una matriz (dimensiones correctas)
+                @filas.times do |i|
+                        raise ArgumentError, 'Los elementos pasados no se corresponden con una matriz' unless @elementos[i].length == @columnas
+                end
+        end
+        
 end
 
